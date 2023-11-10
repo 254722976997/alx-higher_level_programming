13c6b7ca7a0c83560a4fb5acb6468bf3498db28d
Martin Mwangi <martin.hgvdriver@gmail.com>
2023-11-10 09:53:47 -0800

    doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,8 +2,10 @@
 
 /**
  * print_dlistint - Prints all elements of a doubly linked list.
+ * dlistint_t list
+ *
  * @h: Pointer to the head of the list.
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
