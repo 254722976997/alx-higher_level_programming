e7ca97dc503e8437ac0723b3c6d7afbf171c7dde
254722976997 <martin.hgvdriver@gmail.com>
2024-05-19 22:56:51 +0300

    0x10-python-network_0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays only the status code of the response.
+# sends a request to a URL passed as an argument and displays only the status code response
 curl -s -o /dev/null -w "%{http_code}" "$1"
