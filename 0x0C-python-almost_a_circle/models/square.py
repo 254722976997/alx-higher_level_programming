dc94e8f278c7ffde06d7206d93f13c05ef68a7e1
254722976997 <martin.hgvdriver@gmail.com>
2024-05-22 23:15:33 +0300

    python-almost-a-circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,74 +1,64 @@
 #!/usr/bin/python3
+"""Define Square class implement Rectangle
 """
-    contains class Square implements class Rectangle
-"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-        Square implements rectangle
-    """
+    """Square class body
+"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """
-            initialises Square (overrides Rectangle init)
+        """Initialization class props in constructor
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """
-            returns the size of the square
+        """ return width size
         """
         return self.width
 
     @size.setter
     def size(self, value):
+        """module Square height and width
         """
-            sets the value of size
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """
-            assigns key/value argument to attributes
-            kwargs is skipped if args is not empty
-            Args:
-                *args -  variable number of no-keyword args
-                **kwargs - variable number of keyworded args
-        """
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
-
-        try:
-            self.id = args[0]
-            self.size = args[1]
-            self.x = args[2]
-            self.y = args[3]
-        except IndexError:
-            pass
-
     def __str__(self):
+        """Square class string
         """
-            Overloading str function
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
+
+    def update(self, *args, **kwargs):
+        """update square props
         """
-        return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
-                                             self.id, self.x, self.y,
-                                             self.width)
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
+        """ return dict of class props
         """
-            Returns the dictionary representation of a Square
-        """
-        return {'id': getattr(self, "id"),
-                'size': getattr(self, "width"),
-                'x': getattr(self, "x"),
-                'y': getattr(self, "y")}
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
